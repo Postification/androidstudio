b180216de5fd4afc9f6f182a7123b6b8345b416a
s18079 <s18079@tokyo.kosen-ac.jp>
2019-01-12 20:41:07 +0900

    アプリの画面を遷移させるActivity

--- app/src/main/java/com/postification/postification/MainActivity.java
+++ app/src/main/java/com/postification/postification/MainActivity.java
@@ -1,11 +1,16 @@
 package com.postification.postification;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
 
         getData.getPost();
+
+        Button sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
