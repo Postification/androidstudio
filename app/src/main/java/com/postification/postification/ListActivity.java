2ace87634b723b67e3fd5d8bf0c4772beed16fd4
s18079 <45037471+s18079@users.noreply.github.com>
2019-01-16 15:19:29 +0900

    ListViewを追加

--- app/src/main/java/com/postification/postification/ListActivity.java
+++ app/src/main/java/com/postification/postification/ListActivity.java
@@ -3,10 +3,21 @@ package com.postification.postification;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class ListActivity extends AppCompatActivity {
+
+    private static final String[] texts = {
+            "abc ", "bcd", "cde", "def", "efg",
+            "fgh", 	"ghi", "hij", "ijk", "jkl",
+            "klm"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +32,15 @@ public class ListActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        ListView listView = new ListView(this);
+        setContentView(listView);
+
+        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
+
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, texts);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
