f0b857aba00972aefd68141e1ea9a8ea7f4ed255
s18079 <s18079@tokyo.kosen-ac.jp>
2019-01-23 01:50:03 +0900

    ListActivityのボタンを削除

--- app/src/main/java/com/postification/postification/ListActivity.java
+++ app/src/main/java/com/postification/postification/ListActivity.java
@@ -2,13 +2,9 @@ package com.postification.postification;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 
 
 public class ListActivity extends AppCompatActivity {
@@ -25,18 +21,8 @@ public class ListActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_list);
 
-        Button returnButton = findViewById(R.id.return_button);
-        returnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        ListView listView = new ListView(this);
+         ListView listView = new ListView(this);
         setContentView(listView);
-
-        // simple_list_item_1 は、 もともと用意されている定義済みのレイアウトファイルのID
 
         ArrayAdapter<String> arrayAdapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, texts);
